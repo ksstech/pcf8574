e531fc102f51030b069cabe6d592efbe28624bcf
André Maree <ammaree@users.noreply.github.com>
2025-03-20 17:19:39 +0200

    Remove old APIs, add documentation

--- pcf8574.h
+++ pcf8574.h
@@ -48,9 +48,32 @@ typedef struct __attribute__((packed)) pcf8574_t {
 } pcf8574_t;
 DUMB_STATIC_ASSERT(sizeof(pcf8574_t) == 8);
 
+struct report_t;
+
 // ######################################## Global variables #######################################
 
 // ####################################### Global functions ########################################
+
+/**
+ * @brief
+ * @param[in]	psI2C pointer to basic I2C device control block
+ * @return
+ */
+int	pcf8574Identify(struct i2c_di_t * psI2C);
+
+/**
+ * @brief
+ * @param[in]	psI2C pointer to basic I2C device control block
+ * @return
+ */
+int	pcf8574Config(struct i2c_di_t * psI2C);
+
+/**
+ * @brief
+ * @param[in]	psI2C pointer to basic I2C device control block
+ * @return
+ */
+int	pcf8574Diagnostics(struct i2c_di_t * psI2C);
 
 /**
  * @brief	Check if buffer is dirty, if so write to device.
@@ -64,45 +87,28 @@ int pcf8574Flush(pcf8574_io_t eChan);
  */
 int	pcf8574Verify(pcf8574_io_t eChan);
 
-#if (appNEW_CODE == 0)
-
-void pcf8574SetDirection(pcf8574_io_t eCh, bool Dir);
-
-bool pcf8574GetState(pcf8574_io_t eCh);
-
 /**
- * @brief	Update buffered pin status without writing new status to device
- * @return	1/true if buffer is dirty/changed else 0/false
+ * @brief	Perform configuration, read, write and toggle operations on device
+ * @param[in]	Func enumerated operation function code
+ * @param[in]	eChan virtual channel on which the operation is to be performed
+ * @param[in]	NewState optional state for SET type operations
+ * @return	
  */
-bool pcf8574SetStateLazy(pcf8574_io_t eCh, bool NewState);
-
-/**
- * @brief	Update buffered pin status and [if changed] write status to device
- * @return	1/true if status written else 0/false
- */
-bool pcf8574SetState(pcf8574_io_t eCh, bool NewState);
-
-/**
- * @brief	toggle pin status and write status to device
- * @return	pin status after toggled
- */
-bool pcf8574Toggle(pcf8574_io_t eCh);
-
-#else
+int pcf8574Function(pcf8574func_e Func, u8_t eChan, bool NewState);
 
 /**
  * @brief	Configure eChan specified as INput
  * @param[in]	eChan on which the operation is to be performed
  * @return	erSUCCESS
  */
-//#define pcf8574SetDirection(eChan, Dir)			pcf8574Function(cfgDIR, eChan, Dir)
+#define pcf8574SetDirection(eChan, Dir)			pcf8574Function(cfgDIR, eChan, Dir)
 
 /**
  * @brief	Configure eChan specified as INVerted INput
  * @param[in]	eChan on which the operation is to be performed
  * @return	erSUCCESS
  */
-//#define pcf8574SetInvert(eChan)					pcf8574Function(cfgINV, eChan, 0)
+#define pcf8574SetInvert(eChan)					pcf8574Function(cfgINV, eChan, 0)
 
 /**
  * @brief	Read the state (0/1) of the eChan specified
@@ -116,14 +122,14 @@ bool pcf8574Toggle(pcf8574_io_t eCh);
  * @param[in]	eChan on which the operation is to be performed
  * @return	1/true if buffer is dirty/changed else 0/false
  */
-//#define pcf8574ToggleLazy(eChan)				pcf8574Function(stateTGL_LAZY, eChan, 0)
+#define pcf8574ToggleLazy(eChan)				pcf8574Function(stateTGL_LAZY, eChan, 0)
 
 /**
  * @brief	Toggle the state of a eChan (previously configured as OUTput)
  * @param[in]	eChan on which the operation is to be performed
  * @return	1/true if status written else 0/false
  */
-//#define pcf8574Toggle(eChan)					pcf8574Function(stateTGL, eChan, 0)
+#define pcf8574Toggle(eChan)					pcf8574Function(stateTGL, eChan, 0)
 
 /**
  * @brief	Update buffered pin status without writing new status to device
@@ -142,23 +148,10 @@ bool pcf8574Toggle(pcf8574_io_t eCh);
 #define pcf8574SetState(eChan, NewState)		pcf8574Function(stateSET, eChan, NewState)
 
 /**
- * @brief	Perform configuration, read, write and toggle operations on device
- * @param[in]	Func enumerated operation function code
- * @param[in]	eChan virtual channel on which the operation is to be performed
- * @param[in]	NewState optional state for SET type operations
- * @return	
+ * @brief
+ * @param[in]	psI2C pointer to basic I2C device control block
+ * @return
  */
-int pcf8574Function(pcf8574func_e Func, u8_t eChan, bool NewState);
-
-#endif
-
-//int pcf8574Check(pcf8574_t * psPCF8574);
-int	pcf8574Identify(struct i2c_di_t * psI2C);
-int	pcf8574Diagnostics(struct i2c_di_t * psI2C);
-int	pcf8574Config(struct i2c_di_t * psI2C);
-//void pcf8574InitIRQ(void * pvArg);
-
-struct report_t;
 int pcf8574Report(struct report_t * psR);
 
 #ifdef __cplusplus
